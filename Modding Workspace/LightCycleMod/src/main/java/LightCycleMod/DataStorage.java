e0d6b80cb808d8b7803e6fbc949fd420ff76eeed
moebytes <51514613+moebytes@users.noreply.github.com>
2020-07-17 16:31:42 -0400

    Fixed error handling, moved some logic to the set_time_on_start() function in LightCycleFunctions.class

--- Modding Workspace/LightCycleMod/src/main/java/LightCycleMod/DataStorage.java
+++ Modding Workspace/LightCycleMod/src/main/java/LightCycleMod/DataStorage.java
@@ -3,6 +3,8 @@ package LightCycleMod;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /*
  * Stores data that is mod specific
@@ -10,68 +12,77 @@ import java.util.Scanner;
 
 public class DataStorage{
 
-	private final String json_path  = "lightcyclemod.txt";
-	private final String error_path = "lasterror.txt";
+	private final String json_path          = "lightcyclemod.txt";
+	private final String error_path         = "lasterror.txt";
+	private final Logger logger             = LogManager.getLogger();
+	public  final int    DEFAULT_CYCLE_TIME = LightCycleFunctions.instance.DEFAULT_CYCLE_TIME;
 	
 	//Writes the light cycle time to a file.
-	//TODO: Update to receive an object. Write the object.
-	//TODO: Make error catching cleaner
-	public void write_json(double inc_time_by)
+	public void write_json(double new_length)
 	{
 		try {
 			File file = new File(json_path);
 			FileWriter filewriter = new FileWriter(file);
-			filewriter.write( Double.toString(inc_time_by) );
-			filewriter.close();
+		    filewriter.write( Double.toString( new_length ) );
+		    filewriter.close();
 		}
 		catch(Exception e)
 		{
-		
+		    logger.info( "Could not print to file: " + e );
 			try {
-				File file = new File(error_path);
-				file.createNewFile();
+			    File file = new File(error_path);
+			    file.createNewFile();
 				FileWriter filewriter = new FileWriter(file);
-				filewriter.write("Cannot write JSON to file: " + e);
+                filewriter.write("Cannot write JSON to file: " + e);
 				filewriter.close();
 			}
 			catch(Exception ee)
 			{
-				
+				logger.info( "Could not write error message for file writer: " + ee);
 			}
-			
 		}
-		
 	}
+
 	
 	//Reads the light cycle time from a file named {json_path}
-	//TODO: Make error catching cleaner
+	//Only reads one line that contains the day length stored as a double
 	public double read_json()
 	{
+	    //Try to read the file
 		try
 		{
 			File file = new File(json_path);
 			Scanner file_reader = new Scanner(file);
-			
-			while (file_reader.hasNextLine())
-			{
-				String data = file_reader.nextLine();
-				return Double.parseDouble( data ); 
-			}
-			
+			String data = file_reader.nextLine();
+            file_reader.close();
+            
+			return Double.parseDouble( data );
 		}
+		//If it fails, either the file doesn't exist, the formatting was wrong, or there was another error somewhere else
+		//I should return the default value on a failure
 		catch (Exception e)
 		{
+		    logger.info( "Could not read JSON, catching error: " + e );
 			try {
-				File file = new File(json_path);
-				file.createNewFile();
+				
+				//If the file doesn't exist, create a new file and write the default value to it
+				if ( e.getCause().equals( new NullPointerException() ) )
+				{
+	                File file = new File(json_path);
+	                file.createNewFile();
+                    logger.info("Could not read file, sending default time to server.");
+	                this.write_json( DEFAULT_CYCLE_TIME );
+				}
+				
 			}
 			catch(Exception ee)
 			{
-				
+	            logger.info( "Could not print error message for file deletion & creation: " + e );
+                logger.info("Sending default time to server.");
+                this.write_json( DEFAULT_CYCLE_TIME );
 			}
-			
+            return DEFAULT_CYCLE_TIME;
 		}
-		return 0.0;
 	}
 	
 	public String get_json_path()
